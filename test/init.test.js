1f3b4907ed853c684a45c4d81cff062c848d8ed8
unknown <kiranrajeevkv@gmail.com>
2024-12-30 09:38:33 +0530

    test: add template name validation tests

--- test/init.test.js
+++ test/init.test.js
@@ -71,7 +71,7 @@ describe('init', () => {
         await exec(`node ../../bin/index.js init`, { cwd: tempDir });
         const commandHash = computeSHA256Hash(tempDir);
         expect(commandHash).toEqual(originalHash);
-    })
+    });
 
     test('basic', async () => {
         const originalHash = computeSHA256Hash(path.join(__dirname, '..', 'templates', 'basic'));
@@ -95,7 +95,56 @@ describe('init', () => {
     }, 10000);
 
     test('invalid template name passed', async () => {
-        const { stdout, stderr } = await exec(`node ../../bin/index.js init -t invalid_name`, { cwd: tempDir });
-        expect(stripAnsi(stderr)).toContain(`Template invalid_name does not exist. To see available templates use "qse list".`);
+        const { stdout, stderr } = await exec(`node ../../bin/index.js init -t invalid_template`, { cwd: tempDir });
+        expect(stripAnsi(stderr)).toContain(`Template invalid_template does not exist. To see available templates use "qse list".`);
+    });
+
+    test('invalid template name: >214 characters', async () => {
+        const longName = 'a'.repeat(215);
+        const { stderr } = await exec(`node ../../bin/index.js init -n ${longName}`, { cwd: tempDir });
+        expect(stripAnsi(stderr)).toContain('Invalid package name: name can no longer contain more than 214 characters. Please provide a valid package name.');
+    });
+
+    test('invalid template name: contains uppercase characters', async () => {
+        const { stderr } = await exec(`node ../../bin/index.js init -n InvalidName`, { cwd: tempDir });
+        expect(stripAnsi(stderr)).toContain('Invalid package name: name can no longer contain capital letters. Please provide a valid package name.');
+    });
+
+    test('invalid template name: contains non URL friendly charcters', async () => {
+        const { stderr } = await exec(`node ../../bin/index.js init -n "#invalid name"`, { cwd: tempDir });
+        expect(stripAnsi(stderr)).toContain('Invalid package name: name can only contain URL-friendly characters. Please provide a valid package name.');
+    });
+
+    test('valid template name: <= 214 characters', async () => {
+        const validName = 'a'.repeat(214);
+        await exec(`node ../../bin/index.js init -t basic -n ${validName}`, { cwd: tempDir });
+
+        const packageJsonPath = path.join(tempDir, 'package.json');
+        const packageJsonContent = fs.readFileSync(packageJsonPath, 'utf8');
+        const packageJson = JSON.parse(packageJsonContent);
+
+        expect(packageJson.name).toBe(validName);
+    });
+
+    test('valid template name: lowercase only', async () => {
+        const validName = 'validname';
+        await exec(`node ../../bin/index.js init -n ${validName}`, { cwd: tempDir });
+
+        const packageJsonPath = path.join(tempDir, 'package.json');
+        const packageJsonContent = fs.readFileSync(packageJsonPath, 'utf8');
+        const packageJson = JSON.parse(packageJsonContent);
+
+        expect(packageJson.name).toBe(validName);
+    });
+
+    test('valid template name: URL friendly characters', async () => {
+        const validName = 'valid-name';
+        await exec(`node ../../bin/index.js init -n ${validName}`, { cwd: tempDir });
+
+        const packageJsonPath = path.join(tempDir, 'package.json');
+        const packageJsonContent = fs.readFileSync(packageJsonPath, 'utf8');
+        const packageJson = JSON.parse(packageJsonContent);
+
+        expect(packageJson.name).toBe(validName);
     });
 });
