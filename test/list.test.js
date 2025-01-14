bce550a52b7d053a3c6a2b5a15c7fcd6fc120a60
Ashrockzzz2003 <ashrockzzz2003@gmail.com>
2025-01-14 19:40:29 +0530

    Fix qse list test.

--- test/list.test.js
+++ test/list.test.js
@@ -17,9 +17,9 @@ Available Templates:
 - basic_ts
 - express_pg
 - express_pg_sequelize
+- express_mongo
 - express_mysql
 - express_pg_prisma
-- express_mongo
 - express_oauth_microsoft
 - express_oauth_google\n`;
 
